3fe49c6bd8d1448bb6e1505235834cf33c1347ae
codemobiles <chaiyasit.t@gmail.com>
2020-11-09 14:22:26 +0700

    234

--- cmpos/src/App.js
+++ cmpos/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
-import AlarmIcon from "@material-ui/icons/Alarm";
-import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Demo from "./Demo";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,27 +9,6 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 }));
-
-const Demo = () => {
-  return (
-    <>
-      <IconButton aria-label="delete">
-        <DeleteIcon />
-      </IconButton>
-      <IconButton aria-label="delete" disabled color="primary">
-        <DeleteIcon />
-      </IconButton>
-      <IconButton color="secondary" aria-label="add an alarm">
-        <AlarmIcon />
-      </IconButton>
-      <IconButton color="primary" aria-label="add to shopping cart">
-        <AddShoppingCartIcon />
-      </IconButton>
-
-      <img src="images/cmdev_logo.png" alt="" />
-    </>
-  );
-};
 
 export default function App() {
   const classes = useStyles();
