ca179deb7b599642fea77558e4318ea11f234e20
codemobiles <chaiyasit.t@gmail.com>
2020-11-09 11:05:57 +0700

    1234

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function App() {
   return (
     <div>
       <h1>Hey</h1>
     </div>
-  )
+  );
 }
+
+Header = () => {
+  return (
+    <div>
+      <h1>CodeMobiles</h1>
+    </div>
+  );
+};
