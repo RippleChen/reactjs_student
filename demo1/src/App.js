d15cc6f5d5804b3b782262cf7841d288474be4d3
codemobiles <chaiyasit.t@gmail.com>
2020-11-09 11:44:26 +0700

    1234

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -12,7 +12,7 @@ export default function App() {
 
   var count1 = 0;
   const [count2, setCount2] = React.useState(0);
-
+  const [count3, setCount3] = useState(1)
   return (
     <div>
       <Header title="Lek CodeMobiles" />
