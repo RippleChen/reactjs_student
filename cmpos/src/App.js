8196dd04745b9233a227913f7ea255dbfbca4b87
codemobiles <chaiyasit.t@gmail.com>
2020-11-09 14:57:06 +0700

    1234

--- cmpos/src/App.js
+++ cmpos/src/App.js
@@ -1,4 +1,15 @@
 import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+import Header from "./components/layouts/Header";
+import Menu from "./components/layouts/Menu";
+
+
 import { makeStyles } from "@material-ui/core/styles";
 import { Demo } from "./Demo";
 
