5c2d061d65ec7ecbf29534cf2550169cee7fefd2
codemobiles <chaiyasit.t@gmail.com>
2020-11-10 09:40:03 +0700

    1234

--- cmpos/src/App.js
+++ cmpos/src/App.js
@@ -12,20 +12,62 @@ import Menu from "./components/layouts/Menu";
 import LoginPage from "./components/pages/LoginPage";
 import RegisterPage from "./components/pages/RegisterPage";
 
+// ----
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    display: "flex",
+    justifyContent: "center",
+    padding: theme.spacing(3),
+  },
+}));
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { 500: "#1E88E5" },
+  },
+  status: {
+    danger: "orange",
+  },
+});
+
 export default function App() {
+  const classes = useStyles();
+
   return (
     <Router>
       <Header />
       <Menu />
 
       <Switch>
-        <Route path="/login" component={LoginPage} />
-        <Route path="/register" component={RegisterPage} />
-        <Route
-          exact={true}
-          path="/"
-          component={() => <Redirect to="/login" />}
-        />
+        <ThemeProvider theme={theme}>
+          <div className={classes.root}>
+            <Container className={classes.content} maxWidth={false}>
+              <Route path="/login" component={LoginPage} />
+              <Route path="/register" component={RegisterPage} />
+              <Route
+                exact={true}
+                path="/"
+                component={() => <Redirect to="/login" />}
+              />
+            </Container>
+          </div>
+        </ThemeProvider>
       </Switch>
     </Router>
   );
