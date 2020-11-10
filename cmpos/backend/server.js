e747b2c380490602576f9a309309e166f4f8eaf3
codemobiles <chaiyasit.t@gmail.com>
2020-11-10 11:13:29 +0700

    1234

--- cmpos/backend/server.js
+++ cmpos/backend/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const cors = require("cors");
+// const cors = require("cors");
 
-app.use(cors());
+// app.use(cors());
 app.use(express.static(__dirname + "/uploaded"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/api/v2", require("./api"));
+// app.use("/api/v2", require("./api"));
 
 app.listen(8081, () => {
   console.log("Server is running...");
