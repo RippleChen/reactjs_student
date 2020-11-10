17d1f06f3037fa8f3f26b84d08e5d03878c10389
codemobiles <chaiyasit.t@gmail.com>
2020-11-10 10:49:35 +0700

    1234

--- cmpos/backend/server.js
+++ cmpos/backend/server.js
@@ -1,1 +1,14 @@
-console.log("Hey 666");
+const express = require("express");
+const app = express();
+
+app.get("/login", (req, res) => {
+  res.end("login");
+});
+
+app.get("/", (req, res) => {
+  res.end("Home");
+});
+
+app.listen(8001, () => {
+  console.log("server is running.");
+});
