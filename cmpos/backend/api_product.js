70fce06e2e300748bbca5a53dd0ff1f6b3c18470
codemobiles <chaiyasit.t@gmail.com>
2020-11-11 09:49:35 +0700

    134

--- cmpos/backend/api_product.js
+++ cmpos/backend/api_product.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express();
 
-router.get("/product", (req, res) => {
-  res.end("product");
-});
+router.get(
+  "/product",
+  (req, res, next) => {
+    if (req.query.token == "kan") {
+      next();
+    } else {
+      res.end("unauthorized");
+    }
+  },
+  (req, res) => {
+    res.end("product");
+  }
+);
 
 module.exports = router;
