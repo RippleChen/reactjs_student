fc05b79b220f4d2ee3b238441b3976d2b9fd7d87
codemobiles <chaiyasit.t@gmail.com>
2020-11-11 13:38:16 +0700

    134

---
+++ cmpos/src/actions/login.action.js
@@ -0,0 +1,21 @@
+import { LOGIN_FAILED, LOGIN_FETCHING, LOGIN_SUCCESS } from "../constants";
+
+export const setLoginStateToFetch = () => ({
+  type: LOGIN_FETCHING,
+});
+
+export const setLoginStateToSuccess = (payload) => ({
+  type: LOGIN_SUCCESS,
+  payload,
+});
+
+export const setLoginStatetoFailed = (payload) => ({
+  type: LOGIN_FAILED,
+  payload,
+});
+
+export const login = (value, history)=>{
+    return async dispatch=>{
+        
+    }
+}
