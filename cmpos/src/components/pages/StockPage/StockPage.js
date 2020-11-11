524005458d84d0f85e74b97372eb9707a60195e3
codemobiles <chaiyasit.t@gmail.com>
2020-11-11 14:44:25 +0700

    134

--- cmpos/src/components/pages/StockPage/StockPage.js
+++ cmpos/src/components/pages/StockPage/StockPage.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from "react";
+import * as stockActions from "./../../../actions/stock.action";
+
 
 const StockPage = () => {
   const courses = ["Golang", "Angular", "ReactJS", "VueJS"];
+  const stockReducer = useSelector(({ stockReducer }) => stockReducer);
+  const dispatch = useDispatch();
+
 
   useEffect(() => {
     // dispatch to load product
