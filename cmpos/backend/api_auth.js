89c94cc23d6e07472e2de83d4c3b487ef2bee4e7
codemobiles <chaiyasit.t@gmail.com>
2020-11-11 09:27:04 +0700

    134

--- cmpos/backend/api_auth.js
+++ cmpos/backend/api_auth.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const Users = require("./models/user_schema");
+var bcrypt = require("bcryptjs");
+
+
 router.get("/login", (req, res) => {
   res.end("login");
 });
 
 router.post("/register", async (req, res) => {
   try {
+    req.body.password = await bcrypt.hash(req.body.password, 8)
     const doc = await Users.create(req.body);
     res.json(doc);
   } catch (e) {
