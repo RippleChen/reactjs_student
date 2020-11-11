2956fdf23e367b152347e10c66e5f6e08b3b2d19
codemobiles <chaiyasit.t@gmail.com>
2020-11-11 14:49:23 +0700

    134

--- cmpos/src/components/pages/StockPage/StockPage.js
+++ cmpos/src/components/pages/StockPage/StockPage.js
@@ -1,24 +1,25 @@
 import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import * as stockActions from "./../../../actions/stock.action";
-
 
 const StockPage = () => {
   const courses = ["Golang", "Angular", "ReactJS", "VueJS"];
   const stockReducer = useSelector(({ stockReducer }) => stockReducer);
   const dispatch = useDispatch();
 
-
   useEffect(() => {
     // dispatch to load product
     console.log("Initiallization Stock Page");
+    dispatch(stockActions.getProducts());
   }, []);
 
   return (
     <div>
       <ul>
-        {courses.map((item) => (
-          <li>{item}</li>
-        ))}
+        {stockReducer.result &&
+          stockReducer.result.map((item) => (
+            <li key={item.product_id}>{JSON.stringify(item)}</li>
+          ))}
       </ul>
     </div>
   );
