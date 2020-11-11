500fd6c530177ddbcd7dd0a23ba5e9e275a45257
codemobiles <chaiyasit.t@gmail.com>
2020-11-11 10:01:22 +0700

    134

--- cmpos/backend/api_product.js
+++ cmpos/backend/api_product.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express();
+const { interceptor1, interceptor2, interceptor3 } = require("./interceptorX");
 
 router.get("/product", interceptor1, interceptor2, interceptor3, (req, res) => {
   res.end("product");
