0cf3b4f04c7fbdd1719f5c0251e62b96e550fd92
codemobiles <chaiyasit.t@gmail.com>
2020-11-11 09:58:03 +0700

    134

--- cmpos/backend/api_product.js
+++ cmpos/backend/api_product.js
@@ -1,32 +1,32 @@
 const express = require("express");
 const router = express();
 
-router.get(
-  "/product",
-  (req, res, next) => {
-    if (req.query.token1 == "t1") {
-      next();
-    } else {
-      res.end("unauthorized 1");
-    }
-  },
-  (req, res, next) => {
-    if (req.query.token2 == "t2") {
-      next();
-    } else {
-      res.end("unauthorized 2");
-    }
-  },
-  (req, res, next) => {
-    if (req.query.token3 == "t3") {
-      next();
-    } else {
-      res.end("unauthorized 3");
-    }
-  },
-  (req, res) => {
-    res.end("product");
+const interceptor1 = (req, res, next) => {
+  if (req.query.token1 == "t1") {
+    next();
+  } else {
+    res.end("unauthorized 1");
   }
-);
+};
+
+const interceptor2 = (req, res, next) => {
+  if (req.query.token2 == "t2") {
+    next();
+  } else {
+    res.end("unauthorized 2");
+  }
+};
+
+const interceptor3 = (req, res, next) => {
+  if (req.query.token3 == "t3") {
+    next();
+  } else {
+    res.end("unauthorized 3");
+  }
+};
+
+router.get("/product", interceptor1, interceptor2, interceptor3, (req, res) => {
+  res.end("product");
+});
 
 module.exports = router;
