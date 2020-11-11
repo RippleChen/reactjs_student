01a209f671ebd44ff70419082d8162f8563f11ed
codemobiles <chaiyasit.t@gmail.com>
2020-11-11 11:26:23 +0700

    134

--- cmpos/backend/api_product.js
+++ cmpos/backend/api_product.js
@@ -8,18 +8,18 @@ const formidable = require("formidable");
 const path = require("path");
 const fs = require("fs-extra");
 
-
-
 router.get("/product", jwt.verify, async (req, res) => {
   const products = await Products.find({});
   res.json(products);
 });
 
-
 // Add Product
 router.post("/product", async (req, res) => {
   try {
-   
+    const form = new formidable.IncomingForm();
+    form.parse(req, (error, fields, files) => {
+      res.json({ result: "ok", error, fields, files });
+    });
   } catch (err) {
     res.json({ result: "nok", message: JSON.stringify(err) });
   }
