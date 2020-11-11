af61c64aee72cb84e1329d6e10050a295af145a6
codemobiles <chaiyasit.t@gmail.com>
2020-11-11 11:23:20 +0700

    134

--- cmpos/backend/api_product.js
+++ cmpos/backend/api_product.js
@@ -4,9 +4,25 @@ const { interceptor1, interceptor2, interceptor3 } = require("./interceptorX");
 const jwt = require("./jwt");
 const Products = require("./models/product_schema");
 
+const formidable = require("formidable");
+const path = require("path");
+const fs = require("fs-extra");
+
+
+
 router.get("/product", jwt.verify, async (req, res) => {
   const products = await Products.find({});
   res.json(products);
+});
+
+
+// Add Product
+router.post("/product", async (req, res) => {
+  try {
+   
+  } catch (err) {
+    res.json({ result: "nok", message: JSON.stringify(err) });
+  }
 });
 
 module.exports = router;
