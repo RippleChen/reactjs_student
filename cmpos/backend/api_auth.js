0223ef133ec6aaf76fd5732ca6e02e7bf883a276
codemobiles <chaiyasit.t@gmail.com>
2020-11-11 09:37:44 +0700

    134

--- cmpos/backend/api_auth.js
+++ cmpos/backend/api_auth.js
@@ -3,39 +3,38 @@ const router = express.Router();
 const Users = require("./models/user_schema");
 var bcrypt = require("bcryptjs");
 
-
 router.get("/login", (req, res) => {
-  Users.findOne({username:req.body.username}).then(doc=>{
-    if (doc){
-      isValidPassword = await bcrypt.compare(req.body.password, doc.password)
-      if (isValidPassword){
-        // reply ok
+  Users.findOne({ username: req.body.username })
+    .then(async (doc) => {
+      if (doc) {
+        isValidPassword = await bcrypt.compare(req.body.password, doc.password);
+        if (isValidPassword) {
+          // reply ok
+          res.json({
+            result: "ok",
+            message: "Login successfully",
+          });
+        } else {
+          // reply nok - invalid password
+          res.json({
+            result: "nok",
+            message: "Invalid password",
+          });
+        }
+      } else {
+        // reply nok - invalid username
         res.json({
-          result: "ok",                    
-          message: "Login successfully",
-        });
-      }else{
-        // reply nok - invalid password
-        res.json({
-          result: "nok",                    
-          message: "Invalid password",
+          result: "nok",
+          message: "Invalid username",
         });
       }
-    }else{
-      // reply nok - invalid username
-      res.json({
-        result: "nok",                    
-        message: "Invalid username",
-      });
-    }
-  }).catch(e=>{
-
-  })
+    })
+    .catch((e) => {});
 });
 
 router.post("/register", async (req, res) => {
   try {
-    req.body.password = await bcrypt.hash(req.body.password, 8)
+    req.body.password = await bcrypt.hash(req.body.password, 8);
     const doc = await Users.create(req.body);
     res.json(doc);
   } catch (e) {
