4343e0c7ac11c8e4980a9f3ca22ae157ad9762bb
codemobiles <chaiyasit.t@gmail.com>
2020-11-12 15:08:01 +0700

    1234

--- cmpos/src/components/pages/TransactionPage/TransactionPage.js
+++ cmpos/src/components/pages/TransactionPage/TransactionPage.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import MaterialTable from "material-table";
 
 const TransactionPage = () => {
-  return <div>TransactionPage</div>;
+  return (
+    <div style={{ marginTop: 100 }}>
+      <MaterialTable
+        columns={[
+          { title: "Adı", field: "name" },
+          { title: "Soyadı", field: "surname" },
+          { title: "Doğum Yılı", field: "birthYear", type: "numeric" },
+          {
+            title: "Doğum Yeri",
+            field: "birthCity",
+            lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+          },
+        ]}
+        data={[
+          { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
+        ]}
+        title="Demo Title"
+      />
+    </div>
+  );
 };
 
 export default TransactionPage;
