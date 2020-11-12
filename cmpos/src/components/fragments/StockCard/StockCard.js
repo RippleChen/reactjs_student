99e56e756ff60acf752346a8b35a59f6a28275b3
codemobiles <chaiyasit.t@gmail.com>
2020-11-12 11:26:24 +0700

    1234

--- cmpos/src/components/fragments/StockCard/StockCard.js
+++ cmpos/src/components/fragments/StockCard/StockCard.js
@@ -3,7 +3,15 @@ import Card from "@material-ui/core/Card";
 import { Typography, Grid } from "@material-ui/core";
 
 const StockCard = () => {
-  return <Card>StockCard</Card>;
+  return (
+    <Card>
+      <Grid container style={{ minHeight: 70 }}>
+        <Grid item>
+          <span>2134</span>
+        </Grid>
+      </Grid>
+    </Card>
+  );
 };
 
 export default StockCard;
