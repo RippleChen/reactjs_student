02f940831e5df18835312884b6bc08258fe84763
codemobiles <chaiyasit.t@gmail.com>
2020-11-12 13:35:02 +0700

    1234

--- cmpos/src/actions/shop.action.js
+++ cmpos/src/actions/shop.action.js
@@ -1,3 +1,9 @@
 import { httpClient } from "./../utils/HttpClient";
 
 import { SHOP_UPDATE_ORDER, SHOP_UPDATE_PAYMENT, server } from "../constants";
+
+export const addOrder = () => {};
+
+export const removeOrder = () => {};
+
+export const togglePaymentState = () => {};
