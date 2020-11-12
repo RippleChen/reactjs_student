b4968a8055c7a472c32e623ad603ab50d84c0586
codemobiles <chaiyasit.t@gmail.com>
2020-11-12 15:08:57 +0700

    1234

--- cmpos/src/reducers/index.js
+++ cmpos/src/reducers/index.js
@@ -3,11 +3,11 @@ import loginReducer from "./login.reducer";
 import registerReducer from "./register.reducer";
 import stockReducer from "./stock.reducer";
 import shopReducer from "./shop.reducer";
-// import transactionReducer from './transaction.reducer';
+import transactionReducer from "./transaction.reducer";
 export default combineReducers({
   loginReducer,
   registerReducer,
   stockReducer,
   shopReducer,
-  // transactionReducer
+  transactionReducer,
 });
